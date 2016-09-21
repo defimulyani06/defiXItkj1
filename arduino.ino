33e70cc0721efb9bbec3241c40b255233710d4fd
defimulyani06 <defi6tkj1@gmail.com>
2016-09-20 21:24:23 -0700

    Add files via upload

---
+++ arduino.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode{7,OUTPUT};
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite{7,HIGH]; //Pada pin 7, LED menyala
+delay{4000}; //Tunda 4 sec pada saat nyala
+digitalWrite{7,LOW};Pada pin 7, LED mati
+delay{4000}; //tunda 4 sec pada saat mati
+}
